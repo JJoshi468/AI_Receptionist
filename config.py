449b8ea89e71ee34a43c2044ecd8c8235aaf57e7
Jaya Joshi <58749000+JJoshi468@users.noreply.github.com>
2024-12-04 10:32:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+your_project_name='Project_Jaya_Joshi'
+
+
+# Google Calendar API setup
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+CREDENTIALS_FILE = f'{your_project_name}/causal-tracker-291411-f12576247ce9.json'  # Replace with your credentials.json file path
+
+#mongodb uri
+uri = "mongodb+srv://jaya:root@testcluster.gd27n.mongodb.net/?retryWrites=true&w=majority&appName=TestCluster"
+
+#openai key
+key='sk-*************************'
+
+
